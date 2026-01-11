fcb6f9deb98586642e73bda2bdc8a91367624563
smoriginals <smoriginals@users.noreply.github.com>
2026-01-11 23:18:40 +0530

    add github links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,26 +13,69 @@ function App() {
                 <div className='flex h-full w-full flex-col justify-center gap-2 rounded-md md:flex-row'>
 
                     <div className='flex h-full w-full items-center justify-start rounded-md bg-gray-800 pl-16 md:justify-center md:p-2'>
-                        
-                        <ul className='ulHover list-disc space-y-4 font-bold md:text-3xl'>
-                            <li className=' fadeanim hover:text-blue-600'><a href='https://smoriginals-river.onrender.com' target='_blank'>Instagram Mini App</a></li>
+
+                        <ul className='ulHover list-disc space-y-1 font-bold md:text-2xl'>
+
+
+                            <li className=' fadeanim hover:text-blue-600'><a href='https://smoriginals-river.onrender.com' target='_blank'>Instagram Mini App</a>
+                            </li>
+                            <a href='https://github.com/smoriginals/instagram-mini-clone' target='main' className='hover:text-gray-900'>Github</a>
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/Web-Cam' target='_blank'>CromaKey WebCam</a></li>
+                            <a href='https://github.com/smoriginals/Web-Cam' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://anapp.vercel.app' target='_blank'>E-Commerce Garden UI</a></li>
+                            <a href='https://github.com/smoriginals/anapp' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/Smooth-Nav' target='_blank'>Stripe.com Navigation UI</a></li>
+                            <a href='https://github.com/smoriginals/Smooth-Nav' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/Text-to-Speech' target='_blank'>Text-to-Speech</a></li>
+                            <a href='https://github.com/smoriginals/Text-to-Speech' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/Web-Drump' target='_blank'>WebDrum</a></li>
+                            <a href='https://github.com/smoriginals/Web-Drump' target='main' className='hover:text-gray-900'>Github</a>
+
+                            
+
                         </ul>
                     </div>
 
 
                     <div className='flex h-full w-full items-center justify-start rounded-md bg-gray-800 pl-16 md:justify-center md:p-2'>
-                        <ul className='list-disc space-y-4 font-bold md:text-3xl'>
-                            <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/dropdown' target='_blank'>Regex Based Search</a></li>
+                        <ul className='list-disc space-y-1 font-bold md:text-2xl'>
+                            <li className='fadeanim hover:text-blue-600'>
+                                <a href='https://smoriginals.github.io/dropdown' target='_blank'>
+                                    Regex Based Search
+                                </a>
+                            </li>
+                            <a href='https://github.com/smoriginals/dropdown' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/Noughts-Crosses' target='_blank'>Noggets & Crosses</a></li>
+                            <a href='https://github.com/smoriginals/Noughts-Crosses' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/GeoLocation-Compass' target='_blank'>GeoLocation Compass</a></li>
+                            <a href='https://github.com/smoriginals/GeoLocation-Compass' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/Currency-Converter' target='_blank'>Currency Converter</a></li>
+                            <a href='https://github.com/smoriginals/Currency-Converter' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://smoriginals.github.io/Paint-Web' target='_blank'>WebPaint</a></li>
+                            <a href='https://github.com/smoriginals/Paint-Web' target='main' className='hover:text-gray-900'>Github</a>
+
+
                             <li className='fadeanim hover:text-blue-600'><a href='https://livclock.netlify.app' target='_blank'>LivClock</a></li>
+                            <a href='https://github.com/smoriginals/Clock' target='main' className='hover:text-gray-900'>Github</a>
+
+
                         </ul>
                     </div>
                 </div>
